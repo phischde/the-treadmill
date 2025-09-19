c62cb2d7fc160920d7d3a72c9f4d2a0430b59111
Philippe Schrettenbrunner <mail@phisch.de>
2025-09-19 09:24:30 +0200

    Remove unused JS

--- src/assets/js/init.js
+++ src/assets/js/init.js
@@ -2,5 +2,3 @@
 document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js');
 
-// Make site config available to analytics
-window.siteConfig = JSON.parse(document.getElementById('site-config').textContent);
